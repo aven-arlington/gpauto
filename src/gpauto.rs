2a817c253d83085d5e4781d1794aa4ae6b07f522
Aven Arlington <aven.arlington@gmail.com>
2024-06-02 20:13:46 -0700

    Implemented a recursive search for git repositories

--- src/gpauto.rs
+++ src/gpauto.rs
@@ -1,5 +1,8 @@
 use crate::cli::CliArgs;
+use std::fs;
+use std::io;
 use std::path::PathBuf;
+use walkdir::{DirEntry, WalkDir};
 
 #[derive(Debug, PartialEq)]
 pub struct Application {
@@ -12,8 +15,30 @@ impl Application {
     }
 
     pub fn run(&self) {
-        println!("Current directory: {:?}", self.cur_dir);
+        println!("Current working directory: {:?}", self.cur_dir);
+
+        let repos: Vec<PathBuf> = get_repos(&self.cur_dir);
+        println!("Found repos: {:?}", repos.len());
     }
+}
+
+fn get_repos(dir: &PathBuf) -> Vec<PathBuf> {
+    let mut found_repos: Vec<PathBuf> = vec![];
+    if dir.is_dir() {
+        let found_files: Vec<DirEntry> = WalkDir::new(dir)
+            .follow_links(false)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|f| f.file_name() == ".git")
+            .collect();
+        println!("Found files: {:?}", found_files.len());
+        for file in found_files {
+            if file.file_name() == ".git" {
+                found_repos.push(file.path().to_path_buf());
+            }
+        }
+    }
+    found_repos
 }
 
 #[derive(Default)]
